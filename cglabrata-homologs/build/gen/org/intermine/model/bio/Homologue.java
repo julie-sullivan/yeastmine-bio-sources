34863ed85fa08feb3330c4d6445b3c1c4d78d99d
julie-sullivan <julie@flymine.org>
2018-11-16 10:38:48 +0000

    add build.gradle files. add global additions files

---
+++ cglabrata-homologs/build/gen/org/intermine/model/bio/Homologue.java
@@ -0,0 +1,8 @@
+package org.intermine.model.bio;
+
+public interface Homologue extends org.intermine.model.InterMineObject
+{
+    public java.lang.String getSource();
+    public void setSource(final java.lang.String source);
+
+}
